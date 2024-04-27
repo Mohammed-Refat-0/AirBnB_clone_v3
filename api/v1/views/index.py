eb5629bbf6231d04a4bd22d32c22d03f52bcaf1d
Mohammed Refat <mohammedrefat53@outlook.com>
2024-04-27 18:41:18 +0300

    task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,7 @@
 """ implementation of app_view blueprint for an API """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
@@ -9,3 +10,15 @@ def status():
     """"return 'status: OK' response"""
 
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/api/v1/stats', methods=['GET'], strict_slashes=False)
+def count():
+    """"return a json  response of the number of each objects by type:"""
+
+    return jsonify(amenities=storage.count('Amenity'),
+                   cities=storage.count('City'),
+                   places=storage.count('Place'),
+                   reviews=storage.count('Review'),
+                   states=storage.count('State'),
+                   users=storage.count('User'))
