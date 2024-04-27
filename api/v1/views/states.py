ade1e5a1a5aea5c0437c74466a0d42200901161a
Mohammed Refat <mohammedrefat53@outlook.com>
2024-04-27 23:55:12 +0300

    task 6- fix

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -11,7 +11,8 @@ from models import storage
 def states():
     """return a json list of all states"""
     objects = storage.all(State)
-    return jsonify([object.to_dict() for object in objects.values()])
+    json_list = [object.to_dict() for object in objects.values()]
+    return jsonify(json_list)
 
 
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
@@ -49,9 +50,9 @@ def create():
 
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
-def update(id):
+def put_state(state_id):
     """Updates a State object"""
-    object = storage.get(State, id)
+    object = storage.get(State, state_id)
     if not object:
         abort(404)
 
