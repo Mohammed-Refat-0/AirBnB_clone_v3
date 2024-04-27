067bb05f5a235f68229036d4e7fcc9a8e3f07771
Mohammed Refat <mohammedrefat53@outlook.com>
2024-04-27 18:43:19 +0300

    task 4- fix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -12,7 +12,7 @@ def status():
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/api/v1/stats', methods=['GET'], strict_slashes=False)
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def count():
     """"return a json  response of the number of each objects by type:"""
 
